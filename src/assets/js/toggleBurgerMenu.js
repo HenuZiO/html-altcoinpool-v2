4a4f306077a0dec96c2ea8093f3798279d1941ae
HenuZiO <mshenuz@gmail.com>
2023-05-16 22:10:49 +0300

    WIP (Responsive footer, menu)

--- src/assets/js/toggleBurgerMenu.js
+++ src/assets/js/toggleBurgerMenu.js
@@ -1,3 +1,5 @@
-$('.menu').click(function () {
+$('.header__menu-burger--btn').click(function () {
 	$(this).toggleClass('open');
+	$('.header__menu-burger').toggleClass('active');
+	$('body').toggleClass('no-scroll');
 });
